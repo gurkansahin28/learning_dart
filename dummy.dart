f7cd232573faaa46b1410a17fb0698cec5a96784
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-09 11:13:08 +0300

    Update dummy.dart

--- dummy.dart
+++ dummy.dart
@@ -27,6 +27,93 @@ void main() {
   print("Roll Number: ${student.rollNumber}");
 }
 //---2.
+class Mobile {
+  String? name;
+  String? color;
+  int? price;
+
+  Mobile(this.name, this.color, this.price);
+  // here Mobile() is a named constructor
+  Mobile.namedConstructor(this.name, this.color, [this.price = 0]);
+
+  void displayMobileDetails() {
+    print("Mobile name: $name.");
+    print("Mobile color: $color.");
+    print("Mobile price: $price");
+  }
+}
+
+void main() {
+  var mobile1 = Mobile("Samsung", "Black", 20000);
+  mobile1.displayMobileDetails();
+  var mobile2 = Mobile.namedConstructor("Apple", "White");
+  mobile2.displayMobileDetails();
+}
+//---3.
+class Animal {
+  String? name;
+  int? age;
+
+  // Default Constructor
+  Animal() {
+    print("This is a default constructor");
+  }
+
+  // Named Constructor
+  Animal.namedConstructor(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  // Named Constructor
+  Animal.namedConstructor2(String name) {
+    this.name = name;
+  }
+}
+void main(){
+  // Here animal is object of class Animal.
+  Animal animal = Animal.namedConstructor("Dog", 5);
+  print("Name: ${animal.name}");
+  print("Age: ${animal.age}");
+
+  Animal animal2 = Animal.namedConstructor2("Cat");
+  print("Name: ${animal2.name}");
+}
+//---4.
+import 'dart:convert';
+
+class Person {
+  String? name;
+  int? age;
+
+  Person(this.name, this.age);
+
+  Person.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    age = json['age'];
+  }
+
+  Person.fromJsonString(String jsonString) {
+    Map<String, dynamic> json = jsonDecode(jsonString);
+    name = json['name'];
+    age = json['age'];
+  }
+}
+
+void main() {
+// Here person is object of class Person.
+  String jsonString1 = '{"name": "Bishworaj", "age": 25}';
+  String jsonString2 = '{"name": "John", "age": 30}';
+
+  Person p1 = Person.fromJsonString(jsonString1);
+  print("Person 1 name: ${p1.name}");
+  print("Person 1 age: ${p1.age}");
+
+  Person p2 = Person.fromJsonString(jsonString2);
+  print("Person 2 name: ${p2.name}");
+  print("Person 2 age: ${p2.age}");
+}
+//---5.
 
 //--------------------------------------------
 
